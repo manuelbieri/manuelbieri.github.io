c960a63d516d79c51c4f5105665af6b48f580d29
ManNewton <67072666+ManNewton@users.noreply.github.com>
2020-07-01 20:45:33 +0200

    navbar optimisation

---
+++ navbar.js
@@ -0,0 +1,12 @@
+function navbar() {
+    document.write(
+        '<div class="navbar">\n' +
+        '    <a href="index.html" class="active">Home</a>\n' +
+        '    <a href="aboutme.html">About me</a>\n' +
+        '    <a href="news.html">News</a>\n' +
+        '    <a href="forex.html">ForEx</a>\n' +
+        '    <a href="info.html">Info</a>\n' +
+        '</div>'
+    )
+
+}
