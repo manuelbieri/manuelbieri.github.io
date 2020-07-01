83c1630aa4e204074f8003b0802daaabb09e3a5b
ManNewton <67072666+ManNewton@users.noreply.github.com>
2020-07-01 22:08:33 +0200

    created footer

--- js/content.js
+++ js/content.js
@@ -1,11 +1,19 @@
 function navbar() {
     document.write(
         '<div class="navbar">\n' +
-        '    <a href="../index.html" class="active">Home</a>\n' +
-        '    <a href="../aboutme.html">About me</a>\n' +
-        '    <a href="../news.html">News</a>\n' +
-        '    <a href="../forex.html">ForEx</a>\n' +
-        '    <a href="../info.html">Info</a>\n' +
+        '    <a href="index.html" class="active">Home</a>\n' +
+        '    <a href="aboutme.html">About me</a>\n' +
+        '    <a href="news.html">News</a>\n' +
+        '    <a href="forex.html">ForEx</a>\n' +
+        '    <a href="info.html">Info</a>\n' +
+        '</div>'
+    );
+}
+
+function footer() {
+    document.write(
+        '<div class="footer">\n' +
+        '    <h3>Hand-crafted by Manuel Bieri, 2020</h3>\n' +
         '</div>'
     );
 }
