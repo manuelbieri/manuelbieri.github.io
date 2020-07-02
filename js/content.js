0ad6b395ceaaf3bd5b6d8243c8ef28a58073f35b
ManNewton <67072666+ManNewton@users.noreply.github.com>
2020-07-02 22:13:45 +0200

    added tab ico

--- js/content.js
+++ js/content.js
@@ -1,12 +1,13 @@
 //written by Manuel Bieri, 2020
 
-function meta() {
+function head(icon) {
     document.write(
         '    <meta charset="UTF-8">\n' +
         '    <meta name="viewport" content="width=device-width, initial-scale=1.0">\n' +
         '    <meta http-equiv="X-UA-Compatible" content="IE=edge">\n' +
         '    <meta name="author" content="Manuel Bieri">\n' +
-        '    <meta name="year" content="2020">'
+        '    <meta name="year" content="2020">\n\n' +
+        '    <link rel="icon" href="icon/' + icon + '.ico" type="image/x-icon">'
     );
 }
 
@@ -16,7 +17,7 @@ function navbar(active_item) {
     document.write(
         '<div class="navbar">\n' +
         '    <a href="index.html" class="' + active["index"] + '"><img src="icon/home.png" alt="" class="nimg"> Home</a>\n' +
-        '    <a href="me.html" class="' + active["me"] + '"><img src="icon/about.png" alt="" class="nimg"> Me</a>\n' +
+        '    <a href="me.html" class="' + active["me"] + '"><img src="icon/me.png" alt="" class="nimg"> Me</a>\n' +
         '    <a href="blog.html" class="' + active["blog"] + '"><img src="icon/blog.png" alt="" class="nimg"> Blog</a>\n' +
         '    <a href="forex.html" class="' + active["forex"] + '"><img src="icon/forex.png" alt="" class="nimg"> ForEx</a>\n' +
         '    <a href="info.html" class="' + active["info"] + '"><img src="icon/info.png" alt="" class="nimg"> Info</a>\n' +
