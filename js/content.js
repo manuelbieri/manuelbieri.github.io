8cb1df874121ef0b05958f9cab0c4bd51b77dcc8
ManNewton <67072666+ManNewton@users.noreply.github.com>
2020-07-02 19:27:51 +0200

    Update content.js

--- js/content.js
+++ js/content.js
@@ -1,5 +1,15 @@
 //written by Manuel Bieri, 2020
 
+function meta() {
+    document.write(
+        '    <meta charset="UTF-8">\n' +
+        '    <meta name="viewport" content="width=device-width, initial-scale=1.0">\n' +
+        '    <meta http-equiv="X-UA-Compatible" content="IE=edge">\n' +
+        '    <meta name="author" content="Manuel Bieri">\n' +
+        '    <meta name="year" content="2020">'
+    );
+}
+
 function navbar() {
     document.write(
         '<div class="navbar">\n' +
@@ -17,15 +27,5 @@ function footer() {
         '<div class="footer">\n' +
         '    Hand-crafted by Manuel Bieri, 2020 (GPL V3)\n' +
         '</div>'
-    );
-}
-
-function meta() {
-    document.write(
-        '    <meta charset="UTF-8">\n' +
-        '    <meta name="viewport" content="width=device-width, initial-scale=1.0">\n' +
-        '    <meta http-equiv="X-UA-Compatible" content="IE=edge">\n' +
-        '    <meta name="author" content="Manuel Bieri">\n' +
-        '    <meta name="year" content="2020">'
     );
 }
