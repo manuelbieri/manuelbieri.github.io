bdd68259b80486a88bfbe6c9b719066a51c6c19c
ManNewton <67072666+ManNewton@users.noreply.github.com>
2020-07-02 19:26:27 +0200

    update main

--- js/content.js
+++ js/content.js
@@ -19,3 +19,13 @@ function footer() {
         '</div>'
     );
 }
+
+function meta() {
+    document.write(
+        '    <meta charset="UTF-8">\n' +
+        '    <meta name="viewport" content="width=device-width, initial-scale=1.0">\n' +
+        '    <meta http-equiv="X-UA-Compatible" content="IE=edge">\n' +
+        '    <meta name="author" content="Manuel Bieri">\n' +
+        '    <meta name="year" content="2020">'
+    );
+}
