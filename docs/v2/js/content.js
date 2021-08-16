799f3925e6b8edc7de88663c41b585df2953d915
manuelbieri <manuel.bieri@outlook.com>
2021-08-16 23:53:03 +0200

    Improve structure and add images and basic content.
    Signed-off-by: manuelbieri <manuel.bieri@outlook.com>

--- docs/v2/js/content.js
+++ docs/v2/js/content.js
@@ -1,38 +1,37 @@
-function header(title, intro) {
+function navbar(title) {
     document.write(
         '<nav class="navbar navbar-expand-sm navbar-light bg-light">\n' +
         '  <div class="container-fluid">\n' +
-        '    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">\n' +
+        '    <button class="navbar-toggler ml-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">\n' +
         '      <span class="navbar-toggler-icon"></span>\n' +
         '    </button>\n' +
         '    <div class="collapse navbar-collapse" id="navbarNavDropdown">\n' +
         '      <ul class="navbar-nav mx-auto">\n' +
         '        <li class="nav-item">\n' +
-        '          <a class="nav-link" href="index.html">Home</a>\n' +
+        '          <a class="nav-link' + (title === 'Home' ? ' active' : "") + '" href=\"index.html\">Home</a>\n' +
         '        </li>\n' +
         '        <li class="nav-item">\n' +
-        '          <a class="nav-link" href="blog.html">Blog</a>\n' +
+        '          <a class="nav-link' + (title === 'Blog' ? ' active' : "") + '" href="blog.html">Blog</a>\n' +
         '        </li>\n' +
         '        <li class="nav-item">\n' +
-        '          <a class="nav-link" href="projects.html">Projects</a>\n' +
+        '          <a class="nav-link' + (title === 'Projects' ? ' active' : "") + '" href="projects.html">Projects</a>\n' +
         '        </li>\n' +
         '        <li class="nav-item">\n' +
-        '          <a class="nav-link" href="cv.html">CV</a>\n' +
+        '          <a class="nav-link' + (title === 'Curriculum Vitae' ? ' active' : "") + '" href="cv.html">CV</a>\n' +
         '        </li>\n' +
         '      </ul>\n' +
         '    </div>\n' +
         '  </div>\n' +
-        '</nav>\n' +
-        '<br>\n' +
-        headings(title, intro)
+        '</nav>\n'
     )
 }
 
-function headings(title, intro) {
-    return '<h1 class="h3">' + title + '</h1>\n' +
+function header(title, intro) {
+    document.write(
+        '<h1 class="h3">' + title + '</h1>\n' +
         '<br>\n' +
-        '<p>' + intro + '</p>' +
-        '<br>'
+        '<p>' + intro + '</p>'
+    )
 }
 
 function footer() {
@@ -43,6 +42,7 @@ function footer() {
 
 function articleTitle(title) {
     document.write(
+        '<br>' +
         '<h5 class="h5 articleTitle">' + title + '</h5>\n'
     )
 }
@@ -57,7 +57,28 @@ function articleImage(rel_path, text) {
     document.write(
         '<div class="text-center">\n' +
         '   <img src="img/' + rel_path + '" class="img-fluid" alt="Image not available">\n' +
-        '   <p class="text-center"><i>' + text + '</i></p>' +
+        '   <p class="text-center caption"><i>' + text + '</i></p>' +
         '</div>\n'
+    )
+}
+
+function articleGithubTable(url, vcs, releases, version, docs, docs_title = 'Docs') {
+    document.write(
+        '<table class="table">' +
+        '   <thead>' +
+        '       <tr>' +
+        '           <th>VCS</th>' +
+        '           <th>Latest Release</th>' +
+        '           <th>Documentation</th>' +
+        '       </tr>' +
+        '   </thead>' +
+        '   <tbody>' +
+        '       <tr>' +
+        '           <td><a href="' + url + '">' + vcs + '</a></td>' +
+        '           <td><a href="' + releases + '">' + version + '</a></td>' +
+        '           <td><a href="' + docs + '">' + docs_title + '</a></td>' +
+        '           </tr>' +
+        '   </tbody>' +
+        '</table>'
     )
 }
