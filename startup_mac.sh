a096ab4687056a3df3f03cd7069f19867a4f39e7
Manuel Bieri <mail@manuelbieri.ch>
2023-03-24 23:38:58 +0100

    Clean up files.

--- startup_mac.sh
+++
@@ -1,6 +0,0 @@
-ps aux |grep jekyll |awk '{print $2}' | xargs kill -9
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-source ~/.bashrc
-rbenv rehash
-bundle exec jekyll serve
